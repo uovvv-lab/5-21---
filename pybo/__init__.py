a65d5eae4d4f265326d772b736df75c0df008566
김한호 <hanhokimar@gmail.com>
2021-05-14 14:53:23 +0900

    pybo.py추가

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -7,25 +7,23 @@ import config
 db = SQLAlchemy()
 migrate = Migrate()
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    #sqlalcehmy db설정
-    app.config.from_object(config)
-    db.init_app(app)
-    migrate.init_app(app,db)
+#sqlalcehmy db설정
+app.config.from_object(config)
+db.init_app(app)
+migrate.init_app(app,db)
 
-    from . import models
+from . import models
 
-    from .views import main_views,naver_views,question_views,answer_views,auth_views,movie_views
-    app.register_blueprint(main_views.bp)
-    app.register_blueprint(naver_views.bp)
-    app.register_blueprint(question_views.bp)
-    app.register_blueprint(answer_views.bp)
-    app.register_blueprint(auth_views.bp)
-    app.register_blueprint(movie_views.bp)
+from .views import main_views,naver_views,question_views,answer_views,auth_views,movie_views
+app.register_blueprint(main_views.bp)
+app.register_blueprint(naver_views.bp)
+app.register_blueprint(question_views.bp)
+app.register_blueprint(answer_views.bp)
+app.register_blueprint(auth_views.bp)
+app.register_blueprint(movie_views.bp)
 
-    from .filter import format_datetime
-    app.jinja_env.filters['datetime'] = format_datetime
+from .filter import format_datetime
+app.jinja_env.filters['datetime'] = format_datetime
 
-    return app
